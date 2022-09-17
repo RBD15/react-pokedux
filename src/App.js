0540871eef859dc5a6aed5afc21771b46ef0e9a7
RDStudio <nothinghere1984@gmail.com>
2022-09-17 13:42:56 -0400

    [ADD] Redux to handle global state

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import './App.css';
 import Main from './containers/Main';
 import {getPokemons} from './api/index'
+import { useDispatch, useSelector } from 'react-redux';
 
 function App() {
 
-  const [pokemons,setPokemons] = useState([])
-  useEffect(()=>{
-    getPokemons().then(res=>setPokemons(res))
+  //  const [pokemons,setPokemons] = useState([])
+  //  useEffect(()=>{
+  //    getPokemons().then(res=>setPokemons(res))
+  //  },[]);
 
-  },[]);
+   const dispatch = useDispatch()
+   const pokemons= useSelector(state=>state.pokemons);
+
+   useEffect(()=>{
+     getPokemons().then(res=>dispatch({
+       type:'SET_POKEMONS',
+       payload:res
+     }))
+   },[]);
 
   return (
     <div className="App">
